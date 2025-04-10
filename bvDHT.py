008b8d504f9dc240e3411db39d8d5fb0155ab83f
Logan <lrseward22@gmail.com>
2025-04-10 13:09:36 -0500

    Initial setup

--- bvDHT.py
+++ bvDHT.py
@@ -1,3 +1,16 @@
+from socket import *
+import threading
+
+def get_line(conn):
+    msg = b''
+    while True:
+        ch = conn.recv(1)
+        msg += ch
+        if ch == b'\n' or len(ch) == 0:
+            break
+    return msg.decode().strip()
+
+
 def get():
     pass
 
@@ -22,3 +35,19 @@ def disconnect():
 def update_prev():
     pass
 
+# Initial setup
+port = 12345
+socket = socket(AF_INET, SOCK_STREAM)
+socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+socket.bind( ('', port) )
+socket.listen(16)
+
+print("DHT Running...")
+print("Listening on port:", port)
+
+try:
+    # Spawn a thread that listens
+    # Main thread handles user input
+except KeyboardInterrupt:
+    print("\n DHT Shutting Down...")
+    socket.close()
